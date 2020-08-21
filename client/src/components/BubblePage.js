ab78c8496896904866b5964033ddc3f499c958a3
Dee Downs <deedowns@dees-mbp.home>
2020-08-21 12:03:31 -0400

    made GET request for color data

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,13 +1,34 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import { axiosWithAuth } from '../utils/axiosWithAuth'
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState('')
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+
+  const getColorsData = () => {
+    setIsLoading(true)
+    axiosWithAuth().get('/api/colors')
+    .then(res => {
+      // console.log(res)
+      setColorList(res.data)
+      setIsLoading(false)
+    })
+    .catch(err => {
+      console.log(err)
+      setError(err.message)
+      setIsLoading(false)
+    })
+  }
+
+  useEffect(() => {
+    getColorsData()
+  }, [])
 
   return (
     <>
--- client/src/utils/axiosWithAuth.js
+++ client/src/utils/axiosWithAuth.js
@@ -4,8 +4,8 @@ export const axiosWithAuth = () => {
     return axios.create({
         baseURL: 'http://localhost:5000',
         headers: {
-            Authorization: 'ahuBHejkJJiMDhmODZhZi0zaeLTQ4ZfeaseOGZgesai1jZWYgrTA07i73Gebhu98'
-            // localStorage.getItem('token')
+            Authorization: localStorage.getItem('token')
         }
     })
 }
+
